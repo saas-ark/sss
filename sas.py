64144aa9c6e1c75e3842eeb701194a3ddcfb1e70
saas-ark <bwubta22049@brainwareuniversity.ac.in>
2024-09-06 10:09:55 +0530

    Create sas.py

---
+++ sas.py
@@ -0,0 +1,9 @@
+# Without using a temporary variable
+x = 5
+y = 10
+
+# Swapping the values
+x, y = y, x
+
+print("The value of x after swapping: {}".format(x))
+print("The value of y after swapping: {}".format(y))
